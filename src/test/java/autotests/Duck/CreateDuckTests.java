5861be3fcc32e57155df9d37be149dc96b67e948
KarpovaAntonina <karpova.antonina.u@yandex.ru>
2025-01-19 19:21:16 +0700

    создание утки

--- src/test/java/autotests/Duck/CreateDuckTests.java
+++ src/test/java/autotests/Duck/CreateDuckTests.java
@@ -17,6 +17,51 @@ public class CreateDuckTests extends TestNGCitrusSpringSupport {
     @Test(description = "Создать утку")
     @CitrusTest
     public void successfulCreate(@Optional @CitrusResource TestCaseRunner runner) {
+        String color = "yellow";
+        double height = 0.01;
+        String material = "rubber";
+        String sound = "quack";
+        String wingsState = "FIXED";
 
+        createDuck(runner, color, height, material, sound, wingsState);
+
+        String expectedResponseMessage = "{" +
+                "  \"id\": ${duckId}," +
+                "  \"color\": \"" + color + "\"," +
+                "  \"height\": " + height + "," +
+                "  \"material\": \"" + material + "\"," +
+                "  \"sound\": \"" + sound + "\"," +
+                "  \"wingsState\": \"" + wingsState + "\"" +
+                "}";
+
+        runner.$(http().client("http://localhost:2222")
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .extract(fromBody().expression("$.id", "duckId"))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(expectedResponseMessage));
+    }
+
+    public void createDuck(TestCaseRunner runner, String color, double height, String material, String sound, String wingsState) {
+        runner.$(http().client("http://localhost:2222")
+                .send()
+                .post("/api/duck/create")
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body("{\n  \"color\": \"" + color + "\",\n"
+                        + "  \"height\": " + height + ",\n"
+                        + "  \"material\": \"" + material + "\",\n"
+                        + "  \"sound\": \"" + sound + "\",\n"
+                        + "  \"wingsState\": \"" + wingsState
+                        + "\"\n" + "}"));
+    }
+
+    public void validateResponse(TestCaseRunner runner, String responseMessage) {
+        runner.$(http().client("http://localhost:2222")
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE).body(responseMessage));
     }
 }
