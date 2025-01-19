729d702228867674998bc8f0ad6c34b076555e9e
KarpovaAntonina <karpova.antonina.u@yandex.ru>
2025-01-19 18:36:04 +0700

    заготовка для классов

---
+++ src/test/java/autotests/Duck/DeleteDuck.java
@@ -0,0 +1,22 @@
+package autotests.Duck;
+
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Test;
+
+import static com.consol.citrus.dsl.MessageSupport.MessageBodySupport.fromBody;
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+
+public class DeleteDuck extends TestNGCitrusSpringSupport {
+
+    @Test(description = "Удаление утки")
+    @CitrusTest
+    public void successfulDelete(@Optional @CitrusResource TestCaseRunner runner) {
+
+    }
+}
---
+++ src/test/java/autotests/DuckActions/DuckFly.java
@@ -0,0 +1,22 @@
+package autotests.DuckActions;
+
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Test;
+
+import static com.consol.citrus.dsl.MessageSupport.MessageBodySupport.fromBody;
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+
+public class DuckFly extends TestNGCitrusSpringSupport {
+
+    @Test(description = "Проверить, что утка летает")
+    @CitrusTest
+    public void successfulFly(@Optional @CitrusResource TestCaseRunner runner) {
+
+    }
+}
