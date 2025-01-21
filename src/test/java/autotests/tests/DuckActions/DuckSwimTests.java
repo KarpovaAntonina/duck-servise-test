aaf919453a7aaf488cd0670a87680685eb5c3261
KarpovaAntonina <karpova.antonina.u@yandex.ru>
2025-01-21 21:02:35 +0700

    удалила пример, переделала DuckSwimTests

--- src/test/java/autotests/tests/DuckActions/DuckSwimTests.java
+++ src/test/java/autotests/tests/DuckActions/DuckSwimTests.java
@@ -1,9 +1,9 @@
-package autotests.DuckActions;
+package autotests.tests.DuckActions;
 
+import autotests.clients.DuckActionsClient;
 import com.consol.citrus.TestCaseRunner;
 import com.consol.citrus.annotations.CitrusResource;
 import com.consol.citrus.annotations.CitrusTest;
-import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.testng.annotations.Optional;
@@ -12,13 +12,13 @@ import org.testng.annotations.Test;
 import static com.consol.citrus.dsl.MessageSupport.MessageBodySupport.fromBody;
 import static com.consol.citrus.http.actions.HttpActionBuilder.http;
 
-public class DuckSwimTests extends TestNGCitrusSpringSupport {
+public class DuckSwimTests extends DuckActionsClient {
 
     @Test(description = "Проверить, что утка плавает")
     @CitrusTest
     public void successfulSwim(@Optional @CitrusResource TestCaseRunner runner) {
         createDuck(runner, "yellow", 0.15, "rubber", "quack", "FIXED");
-        runner.$(http().client("http://localhost:2222")
+        runner.$(http().client(yellowDuckService)
                 .receive()
                 .response(HttpStatus.OK)
                 .message()
@@ -31,44 +31,15 @@ public class DuckSwimTests extends TestNGCitrusSpringSupport {
     @Test(description = "Проверить, что несуществующая утка не плывет")
     @CitrusTest
     public void notExistsNotSwim(@Optional @CitrusResource TestCaseRunner runner) {
-        runner.$(http().client("http://localhost:2222")
+        runner.$(http().client(yellowDuckService)
                 .send()
                 .get("/api/duck/action/swim")
                 .queryParam("id", "-1"));
-        runner.$(http().client("http://localhost:2222")
+        runner.$(http().client(yellowDuckService)
                 .receive()
-                .response(HttpStatus.OK)
+                .response(HttpStatus.NOT_FOUND)
                 .message()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body("{  \"message\":\"Duck does not exist\"}"));
-    }
-
-    public void duckSwim(TestCaseRunner runner, String id) {
-        runner.$(http().client("http://localhost:2222")
-                .send()
-                .get("/api/duck/action/swim")
-                .queryParam("id", id));
-    }
-
-    public void validateResponse(TestCaseRunner runner, String responseMessage) {
-        runner.$(http().client("http://localhost:2222")
-                .receive()
-                .response(HttpStatus.OK)
-                .message()
-                .contentType(MediaType.APPLICATION_JSON_VALUE).body(responseMessage));
-    }
-
-    public void createDuck(TestCaseRunner runner, String color, double height, String material, String sound, String wingsState) {
-        runner.$(http().client("http://localhost:2222")
-                .send()
-                .post("/api/duck/create")
-                .message()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body("{\n  \"color\": \"" + color + "\",\n"
-                        + "  \"height\": " + height + ",\n"
-                        + "  \"material\": \"" + material + "\",\n"
-                        + "  \"sound\": \"" + sound + "\",\n"
-                        + "  \"wingsState\": \"" + wingsState
-                        + "\"\n" + "}"));
     }
 }
